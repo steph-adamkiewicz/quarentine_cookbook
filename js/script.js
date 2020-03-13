6fb18010929621aef359310e86392558cac33bb9
Stephanie Adamkiewicz <stephanie.adamkiewicz@gmail.com>
2020-03-13 09:20:53 -0700

    Added all html pages and most content from the google doc
    Main menu css is off, but it is functional.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// function activitySelection () {
+//     let mainMenu = document.querySelector(".main-menu-list")
+//     const count = mainMenu.childElementCount - 1
+//     let children = mainMenu.children
+
+//     for (let child in children) {
+//         // let anchor = children[child]
+//         // console.log(anchor)
+//         // //this is not working yet
+//         // anchor.addEventListener('click', function (event) {
+//         //     console.log(event)
+//         // })
+//     }
+
+//     //loop through child elements
+//         //add listener to each
+//         //if it's the "time tracker" element, mark as active by default
+//     let timeTracker = mainMenu.lastElementChild
+//     timeTracker.firstElementChild.className = "active"
+// }
